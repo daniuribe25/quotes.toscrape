28adf9a9cfb4d2f612bb75aa920c3db30bf10923
Daniel Uribe <dani.uribe25@gmail.com>
2021-01-20 09:28:03 -0500

    add port

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const { createService } = require("./services");
 const app = createService();
+const server_port = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(server_port, () => {
+  console.log(`Node server running on ${server_port}`);
+});
